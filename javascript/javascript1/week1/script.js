10e605105ea4e03c0ed68155bf726d39d48895cc
Olha Sashchuk <olha.sashchuk@gmail.com>
2024-04-23 00:08:57 +0200

    Implemented feedback

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,42 +1,64 @@
 //1. Age-ify (A future age calculator)
 const yearOfBirth = 1986;
 const yearFuture = 2027;
-
 const age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
 
 //2. Goodboy-Oldboy (A dog age calculator)
 const dogYearOfBirth = 2017;
 const dogYearFuture = 2027;
-const dogYear = dogYearFuture - dogYearOfBirth;
+let dogYear = dogYearFuture - dogYearOfBirth;
 const shouldShowResultInDogYears = true;
-
+let yearType;
 if (shouldShowResultInDogYears) {
-   console.log ("Your dog will be " + dogYear*7 + " dog years old in " + dogYearFuture)
-} {
-   console.log ("Your dog will be " + dogYear + " human years old in " + dogYearFuture)
+   yearType = "dog years";
+   dogYear *= 7;
+} else {
+   yearType = "human years";
+   dogYear;
 }
+console.log ("Your dog will be " + dogYear + " " + yearType + " old in " + dogYearFuture)
 
 //3. Housey pricey (A house price estimator)
+let payResult;
 //Peter's house: 8m wide, 10m deep and 10m high. The garden size is 100m2. The house costs 2.500.000. 
-//Julia's house: 5m wide, 11m deep and 8m high. The garden size is 70m2. This house costs 1.000.000.
-let wideHouse = prompt('What is your hourse wide?');
-let deepHouse = prompt('What is your hourse deep?');
-let highHouse = prompt('What is your hourse high?');
-let gardenSizeInM2 = prompt('What is your garden size in m2?');
-let priceHouseAd = prompt('What is your houses\'s price?');
-const volumeInMeters = wideHouse*deepHouse*highHouse; 
-const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-if(priceHouseAd>housePrice) {
-   alert ("The real price for this house is " + housePrice + ". You will pay too much for this house.")
-} {
-   alert ("The real price for this house is " + housePrice + ". You will pay too little for this house.")
+const wideHousePeter = 8;
+const deepHousePeter = 10;
+const highHousePeter = 10;
+const gardenSizeInM2Peter = 100;
+const priceHouseAdPeter = 2500000;
+const volumeInMetersPeter = wideHousePeter * deepHousePeter * highHousePeter; 
+const housePricePeter = volumeInMetersPeter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
+if (priceHouseAdPeter > housePricePeter) {
+   payResult = "much";
+   housePricePeter;
+} else {
+   payResult = "little";
+   housePricePeter;
 }
+console.log (`The real price for this house is ${housePricePeter}. You will pay too ${payResult} for this house.`)
+
+//Julia's house: 5m wide, 11m deep and 8m high. The garden size is 70m2. This house costs 1.000.000.
+const wideHouseJulia = 5;
+const deepHouseJulia = 11;
+const highHouseJulia = 8;
+const gardenSizeInM2Julia = 70;
+const priceHouseAdJulia = 1000000;
+const volumeInMetersJulia = wideHouseJulia * deepHouseJulia * highHouseJulia; 
+const housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
+if (priceHouseAdJulia > housePriceJulia) {
+   payResult = "much";
+   housePriceJulia;
+} else {
+   payResult = "little";
+   housePriceJulia;
+}
+console.log (`The real price for this house is ${housePriceJulia}. You will pay too ${payResult} for this house.`)
 
 //4. Ez Namey (Startup name generator)
-firstWords = ["Easy", "Awesome", "Corporate", "Innovative", "Global", "Tech", "Bright", "NexGen", "Smart", "Fusion"]
-secondWords = ["Solutions", "Hub", "Labs", "Forge", "Wave", "Space", "Works", "Growth", "Edge", "Pulse"]
+const firstWords = ["Easy", "Awesome", "Corporate", "Innovative", "Global", "Tech", "Bright", "NexGen", "Smart", "Fusion"]
+const secondWords = ["Solutions", "Hub", "Labs", "Forge", "Wave", "Space", "Works", "Growth", "Edge", "Pulse"]
 const randomNumber = Math.floor(Math.random() * 10);
 const nameStartup = firstWords[randomNumber] + " " + secondWords[randomNumber];
-const lengthNameStartup = nameStartup.length-1;
-console.log ("The startup: \"" + nameStartup + "\" contains " + lengthNameStartup + " characters.")
+const lengthNameStartup = nameStartup.length - 1;
+console.log (`The startup: "${nameStartup}" contains ${lengthNameStartup} characters.`)
